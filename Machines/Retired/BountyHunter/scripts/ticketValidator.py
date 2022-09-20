95552a62bba322c6349d0f33334b9d6a699817e1
g2jz <gmartinmendiola@gmail.com>
2022-09-20 23:17:15 +0200

    Comments changed

--- Machines/Retired/BountyHunter/scripts/ticketValidator.py
+++ Machines/Retired/BountyHunter/scripts/ticketValidator.py
@@ -2,6 +2,7 @@
 #Do not distribute this file.
 
 def load_file(loc):
+    # Checks for .md extension and loads file
     if loc.endswith(".md"):
         return open(loc, 'r')
     else:
@@ -9,38 +10,39 @@ def load_file(loc):
         exit()
 
 def evaluate(ticketFile):
-    #Evaluates a ticket to check for ireggularities.
+    #Evaluates a ticket to check for irregularities.
     code_line = None
-    for i,x in enumerate(ticketFile.readlines()):  # i: Numero de la linea         x: Contenido de la linea 
+    for i,x in enumerate(ticketFile.readlines()):  # i: Number of line, x: Content of the line 
         if i == 0:
-            if not x.startswith("# Skytrain Inc"): # Primera linea
+            if not x.startswith("# Skytrain Inc"): # First line content
                 return False
             continue
         if i == 1:
-            if not x.startswith("## Ticket to "):  # Segunda linea
+            if not x.startswith("## Ticket to "):  # Second line content
                 return False
             print(f"Destination: {' '.join(x.strip().split(' ')[3:])}")
             continue
 
-        if x.startswith("__Ticket Code:__"):       # Tercera linea
+        if x.startswith("__Ticket Code:__"):       # Third line content
             code_line = i+1
             continue
 
-        if code_line and i == code_line:           # Cuarta linea
-            if not x.startswith("**"):             # Tiene que empezar con **
+        if code_line and i == code_line:           # Fourth line content
+            if not x.startswith("**"):             # Has to start with **
                 return False
-            ticketCode = x.replace("**", "").split("+")[0] # Tiene que tener un +
-            if int(ticketCode) % 7 == 4:           # El primer operando de la suma tiene que tener resto 4 al dividirlo entre 7        x * 7 + 4 = ticketCode
-                validationNumber = eval(x.replace("**", "")) # Evalua la expresion completa quitando **
-                if validationNumber > 100:         # Hemos conseguido que se evalue por lo que esta validacion no nos importa
+            ticketCode = x.replace("**", "").split("+")[0] # Has to have a + sign
+            if int(ticketCode) % 7 == 4:           # The first operand of the sum needs to have a 4 reminder when divided by 7,  'x * 7 + 4 = ticketCode'
+                validationNumber = eval(x.replace("**", "")) # Evaluates the complete expression after removing the ** signs
+                if validationNumber > 100:         # We have done the eval so this condiotion does not really matter
                     return True
                 else:
                     return False
     return False
 
 def main():
-    fileName = input("Please enter the path to the ticket file.\n")   # Introducimos la ruta de un archivo
-    ticket = load_file(fileName)                   # Comprueba que la extension del archivo sea md
+    # Main function
+    fileName = input("Please enter the path to the ticket file.\n") 
+    ticket = load_file(fileName)          
     #DEBUG print(ticket)
     result = evaluate(ticket) 
     if (result):
@@ -48,5 +50,6 @@ def main():
     else:
         print("Invalid ticket.")
     ticket.close
+
 
 main()
